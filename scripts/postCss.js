1d7a4a5d18aaa9f17a7450f202637f0a365c124b
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2023-05-05 15:28:00 +0700

    chore(postCss): update postCss & dependencies
    - remove rules to remove root
    - fix build issue by adding .toString() when processing map file
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   scripts/postCss.js
    	modified:   src/style-flightline.map

--- scripts/postCss.js
+++ scripts/postCss.js
@@ -1,7 +1,6 @@
 const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 const postcssCustomProperties = require('postcss-custom-properties')
-const removeRules = require('postcss-remove-rules')
 const comments = require('postcss-discard-comments')
 const fs = require('fs')
 
@@ -12,16 +11,11 @@ cssFiles.map(name => fs.readFile(`src/${name}.css`, (err, css) => {
     .use(comments({
       remove: function(comment) { return comment[0] == "@"; }
     }))
-    .use(removeRules({
-      rulesToRemove: {
-        ':root': '*'
-      }
-    }))
     .process(css, { from: `src/${name}.css`, to: `src/${name}.css` })
     .then(result => {
       fs.writeFile(`src/${name}.css`, result.css, () => true)
       if ( result.map ) {
-        fs.writeFile(`src/${name}.map`, result.map, () => true)
+        fs.writeFile(`src/${name}.map`, result.map.toString(), () => true)
       }
     })
   }));
