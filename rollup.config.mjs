781a79b41ca22cc650cd48196ed8a6b7a9c7be76
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2023-05-05 11:33:31 +0700

    perf(es5): Remove legacy es5 bundling resource #44
    Changes to be committed:
    	modified:   README.md
    	deleted:    babel.config.js
    	modified:   demo/index.html
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    rollup.config.js
    	new file:   rollup.config.mjs
    	deleted:    src/es5.js

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+
+const production = !process.env.ROLLUP_WATCH;
+
+const modernConfig = {
+  input: {
+    ['auro-flightline__bundled']: 'src/auro-flightline.js',
+    ['auro-flight-segment__bundled']: 'src/auro-flight-segment.js'
+  },
+  output: {
+    format: 'esm',
+    dir: 'dist/'
+  },
+  plugins: [
+    nodeResolve(),
+    !production &&
+      serve({
+        open: true,
+        openPage: '/docs/'
+      })
+  ]
+};
+
+export default [modernConfig];
